f0f41b0103111ab04105426eee3150a3a81eb05b
WasATrueWarrior <rohitjaatjaat073@gmail.com>
2024-09-14 13:32:46 +0530

    5.1 done

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,73 @@
-use rand::Rng;
-use std::io;
-use std::cmp::Ordering;
 
-fn main() {
-    println!("Hello, world!");
-    println!("Guess the number!");
-    
-    let  secret_number: u32 = rand::thread_rng().gen_range(1..=100);
-    loop {
-    println!("Please input your guess.");
-    let mut guess: String = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line!");
-    let guess : u32 = match guess.trim().parse() {
-        Ok(num) => num,
-        Err(_) => {
-        println!("Please type a number!");
-        continue
-        }
-    };
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
-    }   
-}
-}
+
+// refrence borrow
+
+// refrence borrow
+
+// refrence borrow
+
+
+// fn main () {
+// let s = String::from("hello");
+// let s2 = calculate_length(&s);
+// println!("{}", s2);
+// }
+
+// fn calculate_length(s: &String) -> usize {
+//     s.len()
+// }
+
+// mut refrence
+// fn main () {
+// let mut s = String::from("hello");
+// change(&mut s);
+// println!("{}", s);
+// }
+
+// fn change(s: &mut String) {
+//     s.push_str(", world");
+// }
+
+
+// slice type 
+
+// fn main() {
+//     let s = String::from("hello world");
+//     let hello = &s[0..5];
+//     let world = &s[6..11];
+//     s.clear();  //error
+//     println!("{} {}", hello, world);
+// }
+
+
+
+
+
+//struct 
+
+
+// struct  Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn main() {
+//     let rect1 = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+//     println!("rect1 is {} x {}" , rect1.width , rect1.height); 
+// }
+
+
+// tupple struct 
+// #[derive(Debug)]
+// struct Rectangle (u32, u32);
+
+// fn main() {
+//     let rect1 = Rectangle(30, 50);
+//     println!("rect1 is {} x {} and rectangle is {:?} ", rect1.0 , rect1.1 , rect1); 
+// }
+
+
+//method syntax
